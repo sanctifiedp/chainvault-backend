3b718e188c88549b3309df403a95afa3e0dee897
Adeyi Gbeminiyi <adeyigbeminiyi414@gmail.com>
2026-02-09 00:35:15 +0100

    Add OAuth2 authorization handler for Twitter

--- api/auth/x-start.js
+++ api/auth/x-start.js
@@ -1,1 +1,18 @@
 
+export default function handler(req, res) {
+  const state = Math.random().toString(36).substring(2);
+
+  const authParams = new URLSearchParams({
+    response_type: "code",
+    client_id: process.env.X_CLIENT_ID,
+    redirect_uri: process.env.X_REDIRECT_URI,
+    scope: "users.read tweet.read",
+    state: state,
+    code_challenge: "chainvault_challenge",
+    code_challenge_method: "plain"
+  });
+
+  const authUrl = `https://twitter.com/i/oauth2/authorize?${authParams.toString()}`;
+
+  res.redirect(authUrl);
+}
